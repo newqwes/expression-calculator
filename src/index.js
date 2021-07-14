e8d1cedafea91ba3ef3b679c56b7ad3da0176a9c
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2021-07-14 18:47:35 +0300

    with new Func

--- src/index.js
+++ src/index.js
@@ -4,7 +4,36 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  console.log(expr);
+
+const left = expr.match(/[(]/g);
+const right = expr.match(/[)]/g);
+
+const devZero = expr.match(/\/ 0/g);
+
+if(devZero) throw 'TypeError: Division by zero.';
+
+if(left && right && left.length !== right.length) {
+  throw 'ExpressionError: Brackets must be paired';
+}
+
+if(!left && right) {
+  throw 'ExpressionError: Brackets must be paired';
+}
+if(left && !right) {
+  throw 'ExpressionError: Brackets must be paired';
+}
+
+
+  const a = new Function(`return ${expr}`);
+
+  const result = a();
+
+  if(result === Infinity) throw "TypeError: Division by zero.";
+
+  console.log(a());
+
+  return result;
 }
 
 module.exports = {
